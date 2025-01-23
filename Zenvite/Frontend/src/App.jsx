fa9e065b1fac959389680c5362374b92234c6bc5
Hrk01221 <hrkisdead01221@gmail.com>
2025-01-24 01:15:06 +0600

    upload project

---
+++ Zenvite/Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "./components/Layout/Layout";
+import "./App.css";
+
+function App() {
+  console.log("App component rendered"); // Debug log
+  return <Layout />;
+}
+
+export default App;
---
+++ Zenvite/Frontend/src/pages/Home.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import '../styles/home.css'
+
+import { Container, Row,Col, CardSubtitle} from 'reactstrap';
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import worldImg from '../assets/images/world.png'
+import experienceImg from '../assets/images/experience.png';
+
+import Subtitle from './../shared/Subtitle';
+import SearchBar from '../shared/SearchBar';
+import ServiceList from '../services/ServiceList';
+import FeaturedEventList from '../components/Featured-events/FeaturedEventList'
+
+
+const Home = () => {
+  return (
+  <>
+
+  {/* ============ hero section start =========== */}
+  <section className="hero">
+  <Container>
+    <Row>
+      <Col lg="6">
+        <div className="hero__content">
+          
+          <h1>
+            Simplify Events,
+            Amplify Experiences with{" "}
+            <span className="highlight">Zenvite</span>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quis
+            deleniti, aperiam odio quae aspernatur quaerat temporibus adipisci
+            incidunt eaque quidem veritatis quasi quia velit provident veniam
+            non vero repudiandae!
+          </p>
+        </div>
+      </Col>
+
+
+    </Row>
+  </Container>
+</section>
+  {/* ============ hero section end =========== */}
+  <section>
+    <Container>
+    <SearchBar/>
+
+    </Container>
+  </section>
+
+  <section>
+    <Container>
+      <Row>
+        <Col lg='3'>
+        <h5 className="services__subtitle">What we serve</h5>  
+        <h2 className="services__title">We offer our best services</h2>
+        </Col>
+
+        <ServiceList/>
+
+      </Row>
+    </Container>
+  </section>
+
+  {/* ============features event section start ============*/}
+  <section>
+    <Container>
+      <Row>
+        <Col lg='12' className="mb-5">
+        
+        <h2 className="featured__event-title">Explore Upcomings!</h2>
+        <h6 className="featured__event-title2">Explore the Universe of Events at Your Fingertips.</h6>
+        </Col>
+
+        <FeaturedEventList/>
+
+      </Row>
+    </Container>
+  </section>
+  
+
+  {/* ============features event section end ============*/}
+  
+ 
+
+  </>
+  );
+    
+};
+
+export default Home;
